0e7cc95830ebea43fe7625195f01baf8c18f74a2
Kevinaziz11 <78891650+Kevinaziz11@users.noreply.github.com>
2022-04-21 02:06:37 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
 
+#include "region1.h"
+#include<ostream>
+
+int main() {
+
+	NS_REGION::RegionSalary test = NS_REGION::RegionSalary();
+	std::list < NS_REGION::RegionSalary > x =  test.extractRegionData();
+	std::cout << "testing123123" << std::endl;
+	if (x.empty()) {
+
+		std::cout << "it is empty" << std::endl;
+
+	}
+	std::list<NS_REGION::RegionSalary>::iterator it;
+	for (it = x.begin(); it != x.end(); ++it) {
+		std::cout << it->getStartSalary()<<std::endl;
+	}
+
+
+
+	//std::cout << x.size() << std::endl;
+	return 0;
+}
