1118bf2cca9ac8f0af1b55b2e49a0611be3e8554
Robert Sanchez <rdjsanchez51@gmail.com>
2022-04-22 00:07:34 -0700

    Started on unitTest.h

--- unitTests.h
+++ unitTests.h
@@ -1,1 +1,23 @@
+#ifndef unitTests_h
+#define unitTests_h
 
+#include "Region.h"
+#include "Degree.h"
+
+namespace NS_UTESTS {
+
+	bool RegionSalary_Object_Test(); //tests whether Region Salary objects are created properly
+
+	bool RegionSalary_FileRead_Test(); // tests extractRegionData function
+
+	bool RegionSalary_PercentChange_Test(); //tests setPercentChange function
+
+	bool Degree_Object_Test(); // tests whether Degree objects are created properly
+
+	bool Degree_FileRead_Test(); // tests degreeFileRead function
+
+	bool Degree_Average_Test(); //tests getDegreeAverage function
+
+}
+
+#endif
