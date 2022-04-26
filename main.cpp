8d2c66beb8e24620f8fccb8bc06b19091104174b
Kevinaziz11 <78891650+Kevinaziz11@users.noreply.github.com>
2022-04-25 22:41:43 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
-
-#include "region1.h"
+#include "UserInterface.h"
 #include<ostream>
 
 int main() {
 
-	NS_REGION::RegionSalary test = NS_REGION::RegionSalary();
-	std::list < NS_REGION::RegionSalary > x =  test.extractRegionData();
-	std::cout << "testing123123" << std::endl;
-	if (x.empty()) {
+	//NS_REGION::RegionSalary test = NS_REGION::RegionSalary();
+	//std::vector < NS_REGION::RegionSalary > x = NS_REGION::extractRegionData();
+	//std::cout << "testing123123" << std::endl;
 
-		std::cout << "it is empty" << std::endl;
+	//test.sortData(x);
+	
+	//for (unsigned int i = 0; i<x.size(); i++) {
+	//	std::cout << x.at(i).getSchool() << std::endl;
+	//	std::cout << x.at(i).getRegion() << std::endl;
+	//	std::cout << x.at(i).getStartSalary() << std::endl;
+	//	std::cout << x.at(i).getMidSalary() << std::endl;
+	//	std::cout << x.at(i).getPercentSalaryChange() << std::endl;
+	//}
 
-	}
-	std::list<NS_REGION::RegionSalary>::iterator it;
-	for (it = x.begin(); it != x.end(); ++it) {
-		std::cout << it->getStartSalary()<<std::endl;
-	}
 
-
-
-	//std::cout << x.size() << std::endl;
-	return 0;
+	
+	//NS_REGION::RegionSalary bestinRegion = bestSchoolInRegion("Midwestern", x);
+	//std::cout << std::endl;
+	//std::cout << x.size();
+	//std::cout << bestinRegion.getSchool() << std::endl;
+	//return 0;
+	NS_USERINTERFACE::userInterface();
 }
